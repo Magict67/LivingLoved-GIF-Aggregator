eb398b928bb3b578398c3d39228f639245c3cf40
Magict67 <christopheradonaithomas@gmail.com>
2025-10-20 17:52:21 -0700

    starting the .js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 const GIPHY_API_KEY = UVBmEU4lOuwzYVfQEP5Oal9XcyRfdXSs;
+const GIPHY_API_URL = https://api.giphy.com/v1/gifs/search;
+const results_limit = 15; //15 is enough
+const imageGrid = $ ("#image-grid");
+$(document) .ready(function() {
+    console.log("Website is ready! Listening for search submissions......");
+    $("#gif-search-form") .on("submit", function(event) {
+        event.preventDefault();
+        const searchTerm = $("#search-term") .val().trim();
+        if (searchTerm === "") {
+            alert("Please enter an inspirational term.");
+            return;
+        }
+
